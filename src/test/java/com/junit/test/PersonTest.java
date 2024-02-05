22f3c53a41a02b51e096ba38327475cc9520d482
Adefioye <abdulhakeemadefioye@gmail.com>
2024-02-05 12:50:14 -0600

    Add person class and test for toString

---
+++ src/test/java/com/junit/test/PersonTest.java
@@ -0,0 +1,15 @@
+package com.junit.test;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PersonTest {
+
+    @Test
+    public void WhenPerformToString_returnPersonObject(){
+
+        Person person = new Person("Koko", 45);
+
+        assertThat(person.toString()).isEqualTo("Person{name='Koko', age=45}");
+    }
+}
